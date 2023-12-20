4a2a396c403a48f4de5251cd6da6ca8e7c72f512
lpeliv <92396416+lpeliv@users.noreply.github.com>
2023-12-20 16:12:36 +0100

    Deleted non-existing pages

--- semester-project/Webpage/app/signin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function SignIn() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-10">
-        <h1 className="text-3xl font-bold">Sign In Page</h1>
-      </main>
-    );
-}
