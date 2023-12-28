158528b3443c2b82e2f83d7653c0a7e6739da70c
lpeliv <92396416+lpeliv@users.noreply.github.com>
2023-12-28 20:03:03 +0100

    Updated the text for missing page

--- semester-project/Webpage/app/not-found.jsx
+++ semester-project/Webpage/app/not-found.jsx
@@ -8,7 +8,7 @@ export default function NotFound() {
             <h2 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-8xl text-brand-blue-900 pb-20 font-bold" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', maxWidth: '100%' }}>
                 We apologize!
             </h2>
-            <p className="flex flex-col items-center justify-start gap-5 font-roboto whitespace-break-spaces font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+            <p className="flex flex-col items-center justify-start gap-5 font-roboto whitespace-break-spaces font-bold text-brand-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '0px 0px 4px rgba(0, 0, 0, 0.5)' }}>
                 It seems there is nothing here...</p>
             <div className="my-0">
                 <Image
