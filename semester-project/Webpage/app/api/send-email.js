1758aa48aecdda53fa10f3075ffa6950fc9aafec
lpeliv <92396416+lpeliv@users.noreply.github.com>
2023-12-29 23:24:35 +0100

    Revert "Feedback template added"
    This reverts commit 4b8b62582f93e310d4c7f8f0ca584d1c2338eae0.

--- semester-project/Webpage/app/api/send-email.js
+++ semester-project/Webpage/app/api/send-email.js
@@ -1,35 +1,8 @@
-import nodemailer from 'nodemailer';
-
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email, writing } = req.body;
-
-    const transporter = nodemailer.createTransport({
-      host: 'your-smtp-host',
-      port: 587,
-      secure: false,
-      auth: {
-        user: 'your-smtp-username',
-        pass: 'your-smtp-password',
-      },
-    });
-
-    const mailOptions = {
-      from: email,
-      to: adminEmail,
-      text: `Hello ${name},\n\n${writing}`,
-    };
-    try {
+    if (req.method === 'POST') {
+      const { name, email, writing } = req.body;
       
-      const info = await transporter.sendMail(mailOptions);
-
-      console.log('Email sent:', info);
-
       res.status(200).json({ success: true });
-    } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
   }
