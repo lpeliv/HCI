2b5512d067300905c0f6f510f7eec029223b6b17
lpeliv <92396416+lpeliv@users.noreply.github.com>
2023-12-29 04:37:30 +0100

    Changed the look of the homepage

--- semester-project/Webpage/app/_components/HeroSection.tsx
+++ semester-project/Webpage/app/_components/HeroSection.tsx
@@ -1,41 +1,33 @@
 import Image from "next/image";
-import unrealPreview from "@/public/hero/UnrealGameImage1.png";
+import hammer from "@/public/hero/hammerHD.png";
 import donuts from "@/public/hero/Donut.png";
+import background from "@/public/hero/Screen.png";
+import Link from "next/link";
 
 const HeroSection = () => (
-    <div className="pt-16">
-        <section className="flex flex-col justify-center items-center p-10 mx-auto bg-blue-200">
-            <div className="justify-start gap-5">
-                <h1 className="font-roboto-condensed text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-8xl font-extrabold text-brand-blue-900 whitespace-break-spaces text-center sm:text-center" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+    <div className="pt-20">
+        <section className="relative flex flex-col justify-center items-center pb-48 pt-48 mx-auto bg-blue-900">
+            <div className="absolute inset-0 w-full h-full bg-cover bg-center filter blur-sm  opacity-20">
+                <Image
+                    src={background}
+                    alt="Background Image"
+                    layout="fill"
+                    objectFit="cover"
+                    quality={100}
+                />
+            </div>
+            <div className="relative z-10">
+                <h1 className="font-roboto-condensed text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-8xl 
+                    font-extrabold text-brand-blue-50 text-center sm:text-center p-10 md:p-20 mt-16">
                     Welcome to the GameDev
                 </h1>
-                <Image
-                    src={unrealPreview}
-                    alt="Unreal Preview"
-                    layout="responsive"
-                    className="rounded-md relative max-w-[1000px] w-full aspect-w-4 aspect-h-2"
-                    style={{
-                        objectFit: "cover",
-                    }}
-                />
-                <p className="font-roboto-condensed whitespace-break-spaces whitespace-nowrap text-brand-blue-50 font-bold text-center text-xl sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                    Homepage of the{" "}
-                    <span className=" text-brand-blue-900 whitespace-nowrap">
-                        GameDev
-                    </span>
-                    , indie game studio.
-                </p>
             </div>
         </section>
-        <section className="justify-center items-center p-10 mx-auto bg-blue-300">
-            <div className="flex flex-col items-center justify-start gap-5 font-roboto whitespace-break-spaces font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                <span>
-                    Check our market for{" "}
-                    <p className="font-roboto-condensed font-bold text-brand-blue-900 whitespace-nowrap" style={{ display: 'inline' }}>
-                        free
-                    </p>
-                    {" "}and paid assets!
-                </span>
+
+        <section className="justify-center items-center pb-32 pt-32 bg-blue-800">
+            <div className="flex flex-col items-center justify-start gap-20 font-roboto whitespace-break-spaces 
+            font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5"
+                style={{ textShadow: '0px 3px 4px rgba(0, 0, 0, 0.5)' }}>
                 <Image
                     src={donuts}
                     alt="Unreal Preview"
@@ -45,28 +37,64 @@ const HeroSection = () => (
                         objectFit: "cover",
                     }}
                 />
-                <button className="bg-blue-700 text-brand-blue-50 py-4 px-8 sm:w-2/3 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-full text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl uppercase transition-all duration-300 ease-in-out hover:bg-blue-900 hover:shadow-lg" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                    <a href="/store" className="">Store</a>
-                </button>
+                <p className="font-roboto whitespace-break-spaces text-center font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+                    Check our market for our assets!
+                </p>
+                <Link href="/store">
+                    <span className="uppercase whitespace-nowrap font-roboto-condensed text-base 
+                        px-20 py-7 rounded-lg bg-brand-blue-100 text-brand-blue-900 
+                        hover:bg-brand-blue-300 sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl transition-all duration-300 ease-in-out">
+                        STORE
+                    </span>
+                </Link>
             </div>
         </section>
-        <section className="justify-center items-center p-10 mx-auto bg-blue-200">
-            <p className="font-roboto whitespace-break-spaces text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5">
-                <div className="flex flex-col items-center justify-start gap-5">
-                    <p className="font-roboto whitespace-break-spaces text-center font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                        Browse our {" "}
-                        <span className="font-roboto-condensed font-bold text-brand-blue-900 whitespace-nowrap">
-                            collection
-                        </span>
-                        {" "}and choose what fits your style!
-                    </p>
-                    <div className="relative max-w-[1000px] w-full aspect-w-4 aspect-h-2">
-                    </div>
-                    <button className="bg-blue-700 text-brand-blue-50 py-4 px-8 sm:w-2/3 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-full text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl uppercase font-bold transition-all duration-300 ease-in-out hover:bg-blue-900 hover:shadow-lg">
-                        <a href="/games" className="">GAMES</a>
-                    </button>
+        <section className="justify-center items-center pb-48 pt-48 mx-auto bg-blue-900">
+            <div className="flex flex-col items-center justify-start gap-20 font-roboto whitespace-break-spaces 
+            font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5"
+                style={{ textShadow: '0px 3px 4px rgba(0, 0, 0, 0.5)' }}>
+                <div className="">
+                    <Image
+                        src={background}
+                        alt="Unreal Preview"
+                        layout="responsive"
+                        className="rounded-md relative max-w-[1000px] w-full aspect-w-4 aspect-h-2"
+                        style={{
+                            objectFit: "cover",
+                        }}
+                    />
                 </div>
-            </p>
+                <p className="font-roboto whitespace-break-spaces text-center font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+                    Browse our collection and choose what fits your style!
+                </p>
+                <div className="relative max-w-[1000px] w-full aspect-w-4 aspect-h-2">
+                </div>
+                <Link href="/games">
+                    <span className="uppercase whitespace-nowrap font-roboto-condensed text-base 
+                        px-20 py-7 rounded-lg bg-brand-blue-100 text-brand-blue-900 
+                        hover:bg-brand-blue-300 sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl transition-all duration-300 ease-in-out">
+                        GAMES
+                    </span>
+                </Link>
+            </div>
+        </section>
+        <section className="justify-center items-center pb-48 pt-48 mx-auto bg-blue-800">
+            <div className="flex flex-col items-center justify-start gap-20 font-roboto whitespace-break-spaces 
+            font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5"
+                style={{ textShadow: '0px 3px 4px rgba(0, 0, 0, 0.5)' }}>
+                <p className="font-roboto whitespace-break-spaces text-center font-bold text-brand-blue-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-5" style={{ textShadow: '0px 3px 4px rgba(0, 0, 0, 0.5)' }}>
+                    Give us your thoughts!
+                </p>
+                <div className="relative max-w-[1000px] w-full aspect-w-4 aspect-h-2">
+                </div>
+                <Link href="/contact">
+                    <span className="uppercase whitespace-nowrap font-roboto-condensed text-base 
+                        px-20 py-7 rounded-lg bg-brand-blue-100 text-brand-blue-900 
+                        hover:bg-brand-blue-300 sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl transition-all duration-300 ease-in-out">
+                        FEEDBACK
+                    </span>
+                </Link>
+            </div>
         </section>
     </div>
 );
