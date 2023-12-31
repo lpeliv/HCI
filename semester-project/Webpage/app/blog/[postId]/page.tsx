124425166f9e761a04fec48be8bb34e0b4c951ef
lpeliv <92396416+lpeliv@users.noreply.github.com>
2023-12-31 03:30:04 +0100

    Correctly applyed style to individual blog section

--- semester-project/Webpage/app/blog/[postId]/page.tsx
+++ semester-project/Webpage/app/blog/[postId]/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { useEffect, useState } from 'react';
-import ContentfulData from '@/app/(contentful)/ContentfullData';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS } from '@contentful/rich-text-types';
+import Link from 'next/link';
 
 interface Params {
   postId: string;
@@ -43,30 +43,44 @@ export default function BlogPost({ params }: { params: Params }) {
   }, [params.postId]);
 
   if (contentfulData === null) {
-    return <div className="text-blue-900 p-10 rounded-lg">Loading...</div>;
+    return <div className="text-blue-50 p-10 rounded-lg">Loading...</div>;
   }
 
   return (
     <main className="justify-between items-center pt-16">
-      <section className="justify-center min-h-screen p-10 mx-auto bg-blue-300">
-        <div className="font-roboto-condensed whitespace-break-spaces text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl p-5">
+      <section className="justify-center p-10 mx-auto bg-blue-800">
+        <div className="font-roboto-condensed whitespace-break-spaces 
+        text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl p-5 text-blue-100">
           <div className="flex flex-col items-center justify-start gap-5">
             <div className="relative max-w-[1000px] w-full aspect-w-4 aspect-h-2">
-              <div className="whitespace-break-spaces text-center font-bold text-brand-blue-50 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', maxWidth: '100%' }}>
-                <h1 className="font-extrabold capitalize text-blue-900 text-5xl lg:text-6xl pb-10">
-                  <span className="text-5xl lg:text-6xl">{contentfulData.title}</span>
+              <div className="whitespace-break-spaces" style={{ textShadow: '0px 0px 2px rgba(0, 0, 0, 0.5)' }}>
+                <h1 className="font-extrabold capitalize text-blue-100 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl pb-5">
+                  <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">{contentfulData.title}</span>
                 </h1>
-                <div className="text-4xl lg:text-5xl p-10 text-left bg-blue-200 rounded-lg">
-                  <div>{contentfulData.author}</div>
-                  <p>
+                <div className="bg-gradient-to-br from-blue-700 to-blue-400 p-3 rounded-l m-2 shadow-lg
+                transition-all duration-500 ease-in-out transform shadow-xl text-blue-50 rounded-lg
+                text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-left"
+                  style={{ textShadow: '0px 0px 2px rgba(0, 0, 0, 0.5)' }}>
+                  <div className="font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-blue-100">{contentfulData.author}</div>
+                  <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl m-4">
                     {documentToReactComponents(contentfulData.content, {
                       renderNode: {
-                        [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
+                        [BLOCKS.PARAGRAPH]: (node, children) => <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">{children}</p>,
                       },
                     })}
                   </p>
                 </div>
+                <div className="flex justify-center gap-5">
+                  <Link href="/blog">
+                    <span className="font-bold uppercase whitespace-nowrap font-roboto-condensed text-base 
+                    px-10 py-2 rounded-lg bg-brand-blue-100 text-brand-blue-900 
+                    hover:text-brand-blue-50 hover:bg-brand-blue-900 sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl transition-all duration-300 ease-in-out">
+                      BACK
+                    </span>
+                  </Link>
+                </div>
               </div>
+
             </div>
           </div>
         </div>
