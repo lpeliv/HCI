5e1f19d5beff724532656939d870b097744e11b5
lpeliv <92396416+lpeliv@users.noreply.github.com>
2023-12-31 03:52:59 +0100

    Adjusted Dropdown to show correct active page

--- semester-project/Webpage/components/Dropdown.tsx
+++ semester-project/Webpage/components/Dropdown.tsx
@@ -11,8 +11,12 @@ interface DropdownProps {
 const Dropdown: React.FC<DropdownProps> = ({ menuOpen, onClose }) => {
   const [visible, setVisible] = useState(false);
 
-  const handleLinkClick = (href: string) => {
+  const handleLinkClick = () => {
     onClose();
+  };
+
+  const isActive = (href: string) => {
+    return window.location.pathname === href;
   };
 
   useEffect(() => {
@@ -33,7 +37,7 @@ const Dropdown: React.FC<DropdownProps> = ({ menuOpen, onClose }) => {
 
   return (
     <>
-      { (
+      
         <div
           className={`lg:hidden absolute top-24 right-1 p-1 space-y-2 font-roboto-condensed 
           text-brand-blue-900 rounded-md text-xl overflow-hidden transition-transform duration-500 
@@ -42,51 +46,68 @@ const Dropdown: React.FC<DropdownProps> = ({ menuOpen, onClose }) => {
         >
           <ul className="list-none p-0 m-0 font-bold uppercase">
             <li className="mb-1 text-center">
-              <Link href="/" onClick={() => handleLinkClick('/')}>
-                <span className="rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 bg-brand-blue-50 hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out">
+              <Link href="/" onClick={handleLinkClick}>
+                <span
+                  className={`rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 ${isActive('/') ? 'bg-brand-blue-900 text-blue-50' : 'bg-brand-blue-50'
+                    } hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out`}
+                >
                   Home
                 </span>
               </Link>
             </li>
             <li className="mb-1 text-center">
-              <Link href="/blog" onClick={() => handleLinkClick('/blog')}>
-                <span className="rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 bg-brand-blue-50 hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out">
+              <Link href="/blog" onClick={handleLinkClick}>
+                <span
+                  className={`rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 ${isActive('/blog') ? 'bg-brand-blue-900 text-blue-50' : 'bg-brand-blue-50'
+                    } hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out`}
+                >
                   Blog
                 </span>
               </Link>
             </li>
-
             <li className="mb-1 text-center">
-              <Link href="/about" onClick={() => handleLinkClick('/about')}>
-                <span className="rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 bg-brand-blue-50 hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out">
-                  About Us
+              <Link href="/about" onClick={handleLinkClick}>
+                <span
+                  className={`rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 ${isActive('/about') ? 'bg-brand-blue-900 text-blue-50' : 'bg-brand-blue-50'
+                    } hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out`}
+                >
+                  About
                 </span>
               </Link>
             </li>
             <li className="mb-1 text-center">
-              <Link href="/contact" onClick={() => handleLinkClick('/store')}>
-                <span className="rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 bg-brand-blue-50 hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out">
+              <Link href="/contact" onClick={handleLinkClick}>
+                <span
+                  className={`rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 ${isActive('/contact') ? 'bg-brand-blue-900 text-blue-50' : 'bg-brand-blue-50'
+                    } hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out`}
+                >
                   Contact us
                 </span>
               </Link>
             </li>
             <li className="mb-1 text-center">
-              <Link href="/games" onClick={() => handleLinkClick('/games')}>
-                <span className="rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 bg-brand-blue-50 hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out">
+              <Link href="/games" onClick={handleLinkClick}>
+                <span
+                  className={`rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 ${isActive('/games') ? 'bg-brand-blue-900 text-blue-50' : 'bg-brand-blue-50'
+                    } hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out`}
+                >
                   Games
                 </span>
               </Link>
             </li>
             <li className="mb-1 text-center">
-              <Link href="/store" onClick={() => handleLinkClick('/contact')}>
-                <span className="rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 bg-brand-blue-50 hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out">
-                  store
+              <Link href="/store" onClick={handleLinkClick}>
+                <span
+                  className={`rounded-sm block uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 mx-auto w-32 ${isActive('/store') ? 'bg-brand-blue-900 text-blue-50' : 'bg-brand-blue-50'
+                    } hover:bg-brand-blue-200 text-2xl transition-all duration-300 ease-in-out`}
+                >
+                  Store
                 </span>
               </Link>
             </li>
           </ul >
         </div >
-      )}
+    
     </>
   );
 };
