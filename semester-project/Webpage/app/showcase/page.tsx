55066b74bcea34caae165fd698838741ddc63b59
lpeliv <92396416+lpeliv@users.noreply.github.com>
2024-01-23 22:56:18 +0100

    Minor fix

--- semester-project/Webpage/app/showcase/page.tsx
+++ semester-project/Webpage/app/showcase/page.tsx
@@ -14,7 +14,7 @@ export default async function Store({
           <div className="flex flex-col items-center justify-start gap-5">
             <div className="text-center  font-bold p-5" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
               <h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-8xl text-brand-blue-100 pb-20">
-                Welcome to the showcase
+                Showcase of our assets
               </h1>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {items.map((item: StoreItem) => (
@@ -23,7 +23,7 @@ export default async function Store({
                     className="bg-gradient-to-r from-blue-900 to-blue-700 p-3 rounded-l shadow-lg
                     transition-all duration-500 ease-in-out transform hover:from-blue-200 
                     hover:to-blue-500 hover:shadow-xl hover:scale-110 text-blue-50 hover:text-blue-900"
-                    style={{ textShadow: '0px 0px 2px rgba(0, 0, 0, 0.5)'}}
+                    style={{ textShadow: '0px 0px 2px rgba(0, 0, 0, 0.5)' }}
                   >
                     <h2 className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-5xl">
                       {item.itemName}
